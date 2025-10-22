c5db882427ab368e0cf7b5624a219bcc17421851
Hasan Khan <k250722@nu.edu.pk>
2025-10-22 16:22:50 +0500

    q6.c

--- q6.c
+++ q6.c
@@ -11,15 +11,20 @@ int main()
     for(i = 0; str[i] != '\0'; i++) 
     {
         char ch = str[i];
-
         if (ch >= 'A' && ch <= 'Z')
+        {
             ch = ch + 32;
-
-        if (ch >= 'a' && ch <= 'z') {
+        }
+        if (ch >= 'a' && ch <= 'z') 
+        {
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
                 vowels++;
+            }
             else
+            {
                 consonants++;
+            }
         }
     }
 
@@ -27,4 +32,5 @@ int main()
     printf("Consonants: %d\n", consonants);
 
     return 0;
+
 }
